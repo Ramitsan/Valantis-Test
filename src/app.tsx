d2a999334d84f7dec8e0d169b484bb8150137710
Ramitsan <ramitsan@gmail.com>
2024-02-27 23:46:27 +0300

    implement ids request

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { MD5 } from 'crypto-js';
 // import './style.css';
 
 export default function App() {
+ 
+  useEffect(() => {
+    const date = new Date(); 
+    const month = date.getUTCMonth() + 1 < 10 ? `0${date.getUTCMonth() + 1}` : date.getUTCMonth() + 1;
+    const day = date.getUTCDate() < 10 ? `0${date.getUTCDate()}` : date.getUTCDate();
+    
+    console.log(`Valantis_${date.getUTCFullYear()}${month}${day}`);
+
+    fetch('http://api.valantis.store:40000/', {
+      headers: {
+        'X-Auth': MD5(`Valantis_${date.getUTCFullYear()}${month}${day}`).toString(),
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        "action": "get_ids",
+	      "params": {"offset": 0, "limit": 50}
+      })
+    })
+      .then(res => res.text())
+      .then((data) => console.log(data))
+  }, []);
+
   return (
     <>
-      <div className="wrapper">  <p>12345</p>     
+      <div className="wrapper">  <p>12345</p>
       </div>
     </>
   )
