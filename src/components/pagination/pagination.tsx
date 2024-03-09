db7600727c031e4c296eb5bb6d93858f6512ff25
Ramitsan <ramitsan@gmail.com>
2024-03-09 18:42:31 +0300

    fix styles

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import './pagination.css';
 
 interface IPaginationProps {
   onChange: (value: number) => void,
@@ -9,8 +10,8 @@ interface IPaginationProps {
 export function Pagination({onChange, currentPage, maxPage} : IPaginationProps) {
   return (
     <div className="pagination">
-          <button onClick={() => onChange(Math.max(currentPage - 1, 0))}>Back</button>
-          <button onClick={() => onChange(Math.min(currentPage + 1, maxPage))}>Next</button>
+          <button className="pagination__button pagination__button--back" onClick={() => onChange(Math.max(currentPage - 1, 0))}>Back</button>
+          <button className="pagination__button pagination__button--next" onClick={() => onChange(Math.min(currentPage + 1, maxPage))}>Next</button>
         </div>
   )
 }
